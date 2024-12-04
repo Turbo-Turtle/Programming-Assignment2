0e4a9eec8f3c0e94bde31c3762bdea79f4be1d5f
Logan Bossie <gestalt.yrha@protonmail.com>
2024-12-03 19:43:42 -0500

    Character movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    GameObject Player;
+    public Vector2 position = Vector2.zero;
+    public Vector2 velocity = Vector2.zero;
+    float speed = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    
     {
+
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float dt = Time.deltaTime;
+        position += velocity * dt;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+
+        }
     }
 }
