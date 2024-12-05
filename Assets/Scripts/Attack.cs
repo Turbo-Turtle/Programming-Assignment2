b55b1a8c654b1488f0cd2177bb8a47485a7986ce
cris-tofer <chrisnf05@gmail.com>
2024-12-05 18:20:17 -0500

    perfected movement

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     public GameObject[] bullet;
+    private int i = 0;
     void Start()
     {
 
@@ -10,9 +11,12 @@ public class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKey(KeyCode.Space))
         {
-
+            bullet[i].SetActive(true);
+            bullet[i].transform.position = transform.position;
+            i++;
         }
     }
 }
