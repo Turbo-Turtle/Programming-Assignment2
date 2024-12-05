c6a54f2c5ecfba0c230fd3843645f542979f397f
cris-tofer <chrisnf05@gmail.com>
2024-12-05 12:13:22 -0500

    chaged some prefabs

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject[] bullet;
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/BigAsteroidDeath.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class BigAsteroidDeath : MonoBehaviour
+{
+    void OnCollision2D()
+    {
+
+    }
+}
